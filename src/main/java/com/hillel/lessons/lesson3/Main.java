656163aefbb9229ce326dfae27f9395768e91760
Stanchevskaya <122034686+Stanchevskaya@users.noreply.github.com>
2023-01-05 17:16:36 +0200

    Update Main.java

--- src/main/java/com/hillel/lessons/lesson3/Main.java
+++ src/main/java/com/hillel/lessons/lesson3/Main.java
@@ -3,7 +3,6 @@ package com.hillel.lessons.lesson3;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello git!");
-        System.out.println("hello");
-        System.out.println("hello");
+     
     }
 }
