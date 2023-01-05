fa939c5c9b63c57b4e579d869d19a197e49987aa
Olga Stanchevska <stanchevskaya@gmail.com>
2023-01-05 17:30:59 +0200

    added files lesson 3

--- src/main/java/com/hillel/lessons/lesson3/Main.java
+++ src/main/java/com/hillel/lessons/lesson3/Main.java
@@ -3,6 +3,8 @@ package com.hillel.lessons.lesson3;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello git!");
-     
+        System.out.println("Hello git!");
+
+
     }
 }
