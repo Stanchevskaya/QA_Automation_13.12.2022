ea8eaf9261cc11be82d6ea7269dab614333b1588
Stanchevskaya <stanchevskaya@gmail.com>
2023-01-17 14:35:48 +0200

    Commit

--- src/com/hillel/homework/Homework7.java
+++ src/com/hillel/homework/Homework7.java
@@ -8,23 +8,28 @@ public class Homework7 {
         Random random = new Random();
 
         int number = random.nextInt(11);
-
         Scanner scanner = new Scanner(System.in);
 
         for (int i = 0; i < 3; i++) {
 
             System.out.print("Enter a guess (0-10): ");
-            int guess = scanner.nextInt();
+            if (scanner.hasNextInt()) {
+                int guess = scanner.nextInt();
 
-            if (guess == number) {
-                System.out.println("You win! The number was " + number);
-                return;
-            } else {
+                if (guess == number) {
+                    System.out.println("You win! The number was " + number);
+                    return;
+                } else {
 
                     System.out.println("Try again!");
                 }
+            } else {
+                System.out.println("Invalid input, please enter a number");
+                scanner.next();
             }
-        System.out.println("You lose. The number was " + number);
         }
+        System.out.println("You lose. The number was " + number);
     }
+}
+
 
