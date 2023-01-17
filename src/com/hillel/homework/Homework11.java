94b2b5616740126e2323dce344ddca999135320f
Stanchevskaya <stanchevskaya@gmail.com>
2023-01-17 14:21:14 +0200

    Commit

--- src/com/hillel/homework/Homework11.java
+++ src/com/hillel/homework/Homework11.java
@@ -1,4 +1,18 @@
 package com.hillel.homework;
 
 public class Homework11 {
-}
+    public static void main(String[] args) {
+
+    class Person {
+        public String personInfo(String name, String surname, String city, String phone) {
+            return "Зателефонувати громадянину " + name + " " + surname + " із міста " + city + " можна за номером " + phone;
+        }
+    }
+            Person person1 = new Person();
+            Person person2 = new Person();
+            Person person3 = new Person();
+            System.out.println(person1.personInfo("Will", "Smith", "New York", "2936729462846"));
+            System.out.println(person2.personInfo("Jackie", "Chan", "Shanghai", "12312412412"));
+            System.out.println(person3.personInfo("Sherlock", "Holmes", "London", "37742123513"));
+        }
+    }
